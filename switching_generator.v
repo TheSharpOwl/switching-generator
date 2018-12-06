87b9a716e791f3ae02f487649cabdad558627f03
Silver <ziadxkabakibi@users.noreply.github.com>
2018-12-06 11:13:18 +0300

    Add files via upload

--- switching_generator.v
+++ switching_generator.v
@@ -1,25 +1,58 @@
-module switching_generator
+module switching_generator//the switching generator
 #(parameter N, M)
 (
 	clk,
+	seed,
+	set,
 	out
 );
 
 input clk;
+input set;
+input[0: N + 3 * M - 1] seed;
 
 output out;
 
-wire [0: N - 1] a_lfsr_set;
-wire ctrl_set;
-wire ctrl_out;
+reg [0: M * M - 1] M1;
+reg [0: M * M - 1] M2;
 
-control_unit #(N) ctrl(clk, a_lfsr_set, ctrl_set, ctrl_out);
+wire [0: M * M - 1] data_trans_seed1;
+wire [0: M * M - 1] data_trans_seed2;
 
-wire [0: M - 1] b_lfsr_set;
-wire [0: M * M - 1] b_trans_1_mat_set;
-wire [0: M * M - 1] b_trans_2_mat_set;
-wire data_set;
+wire data_ctrl;
 
-data_unit #(M) data(clk, ctrl_out, b_lfsr_set, b_trans_1_mat_set, b_trans_2_mat_set, data_set, out);
+matrix_power #(M, M) mp1
+(
+	M1,
+	seed[N + M: N + 2 * M - 1],
+	data_trans_seed1
+);
+
+matrix_power #(M, M) mp2
+(
+	M2,
+	seed[N + 2 * M: N + 3 * M - 1],
+	data_trans_seed2
+);
+
+control_unit #(N) cu_inst
+(
+	clk,
+	seed[0: N - 1],
+	set,
+	data_ctrl
+);
+
+data_unit #(M) du_inst
+(
+	clk,
+	data_ctrl,
+	seed[N: N + M - 1],
+	data_trans_seed1,
+	data_trans_seed2,
+	set,
+	out
+);
+
 
 endmodule
